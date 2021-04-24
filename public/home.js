32b6486d9124e6db0d2341b9da4cc062a5c68d7a
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-04-24 16:25:25 +0900

    ask for name and paint function

--- public/home.js
+++ public/home.js
@@ -1,2 +1,30 @@
-const h1 = document.querySelector("h1");
-h1.innerHTML = "Welcome"
+const jsForm = document.querySelector(".js-form"),
+    jsInput = jsForm.querySelector("input"),
+    jsHello = document.querySelector(".hello")
+
+const USER_LS = "currentUser";
+
+function paintGreeting(text) {
+    jsHello.className = "showing";
+    jsHello.innerHTML = `Hello ${text}!`;
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = jsInput.value;
+    jsInput.value = "";
+    paintGreeting(currentValue);
+    jsForm.className = "removing";
+    localStorage.setItem(USER_LS, currentValue);
+}
+
+// 1st. User localStorage Data Loading
+const currentUser = localStorage.getItem(USER_LS);
+
+// 2nd. Checking 
+if (currentUser) {
+    // user Data exists
+} else {
+    jsForm.className ="showing";
+    jsForm.addEventListener("submit", handleSubmit);
+}
