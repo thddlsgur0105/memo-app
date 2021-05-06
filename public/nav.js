03af155340963e132ab2c7af8106ef7eb164333f
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-06 18:44:07 +0900

    Animation on btn

--- public/nav.js
+++ public/nav.js
@@ -1,25 +1,24 @@
-const jsNavMain = document.querySelector(".nav__main"),
+const jsNavContainer = document.querySelector(".nav"),
+      jsNavMain = document.querySelector(".nav__main"),
       jsNavIcon = jsNavMain.querySelector("i"),
-      jsNavSub = document.querySelector(".nav__sub");
+      jsNavSub = jsNavContainer.querySelector(".nav__sub");
 
 function handleNavEnter() {
     jsNavIcon.classList.replace("far", "fas");
 
     //SUB
-    jsNavSub.classList.replace("removing", "showing");
-    jsNavSub.classList.add("left-top-position");
+    jsNavSub.classList.add("move")
 }
 
 function handleNavLeave() {
     jsNavIcon.classList.replace("fas", "far");
 
     //SUB
-    jsNavSub.classList.replace("showing", "removing");
 }
 
 function init() {
-    jsNavMain.addEventListener("mouseenter", handleNavEnter);
-    jsNavMain.addEventListener("mouseleave", handleNavLeave);
+    jsNavContainer.addEventListener("mouseenter", handleNavEnter);
+    jsNavContainer.addEventListener("mouseleave", handleNavLeave);
 }
 
 init();
