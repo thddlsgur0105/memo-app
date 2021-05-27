19799c25c58a1e3255e2d5cec027c3f41585e808
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-27 18:46:50 +0900

    활용

--- src/controllers/memoController.js
+++ src/controllers/memoController.js
@@ -5,7 +5,7 @@ export const getAddMemo =(req, res) => {
     return res.render("addMemo", { pageTitle: "addMemo" })
 };
 
-// 미완
+// 완료
 export const postAddMemo = async (req, res) => {
     const { title, description, hashtags } = req.body;
     const { user } = req.session;
@@ -55,15 +55,19 @@ export const getEditMemo = async (req, res) => {
     return res.render("editMemo", { pageTitle: "editMemo", toDo: editToDo });
 };
 
-// 미완
+// 미완 
+// $set 활용해서 우선순위도 설정해주는 알고리즘으로 작성 
 export const postEditMemo = async (req, res) => {
     const { id } = req.params;
-    const { title, description, hashtags } = req.body;
+    const { title, description, hashtags, completed } = req.body;
     const { user } = req.session;
     await ToDo.findByIdAndUpdate(id, {
         title,
         description,
         hashtags: addHashtags(hashtags),
+        $set: {
+            meta: { completed }
+        },
     });
     return res.redirect(`/users/${user._id}/memo`);
 };
