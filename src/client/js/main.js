653419a730288330418acd81921db78144fd9cac
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-06-11 02:05:20 +0900

    frontend 임시완료

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -12,7 +12,7 @@ let memoArray = [];
 
 function saveMemo(obj) {
     memoArray.push(obj);
-    localStorage.setItem("myMemo", JSON.stringify(memoArray));
+    localStorage.setItem("toDos", JSON.stringify(memoArray));
 }
 
 function handleDeleteBtnClick(event) {
@@ -23,13 +23,104 @@ function handleDeleteBtnClick(event) {
     }
     const targetMemo = targetNode.parentNode
     const targetId = targetMemo.id;
+
     // Delete target in Frontend
     targetMemo.remove();
 
     // Delete target in Backend
-    // 기존의 localStorage 내용 로드
     memoArray = memoArray.filter(oneMemo => oneMemo.id !== parseInt(targetId))
-    localStorage.setItem("myMemo", JSON.stringify(memoArray))
+    localStorage.setItem("toDos", JSON.stringify(memoArray))
+}
+
+function handleOptionBtnClick(event) {
+    event.preventDefault();
+    let targetNode = event.target;
+    if (targetNode.tagName === "I") {
+        targetNode = targetNode.parentNode;
+    }
+    targetNode.classList.toggle("clicked");
+    const targetContainer = targetNode.parentNode;
+
+    if (targetNode.classList.contains("clicked")) {
+        // show all option Btns
+        const targetHideBtns = targetContainer.querySelectorAll("button.hide");
+        targetHideBtns.forEach(targetHideBtn => {
+            targetHideBtn.classList.replace("hide", "show");
+        })
+
+        // targetOptionBtn direction changes to left
+        const targetIcon = targetNode.querySelector("i")
+        targetIcon.classList.replace("fa-chevron-right", "fa-chevron-left")
+    } else {
+        // show all option Btns
+        const targetHideBtns = targetContainer.querySelectorAll("button.show");
+        targetHideBtns.forEach(targetHideBtn => {
+            targetHideBtn.classList.replace("show", "hide");
+        })
+
+        // targetOptionBtn direction changes to right
+        const targetIcon = targetNode.querySelector("i")
+        targetIcon.classList.replace("fa-chevron-left", "fa-chevron-right")
+    }
+}
+
+function handleEditBtnClick(event) {
+    event.preventDefault();
+    let targetNode = event.target;
+    if (targetNode.tagName === "I") {
+        targetNode = targetNode.parentNode;
+    }
+    targetNode.classList.toggle("clicked");
+    const targetContainer = targetNode.parentNode;
+
+    if (targetNode.classList.contains("clicked")) {
+        // replace memo title to input bar
+        const titleContainer = targetContainer.querySelector(".memo__title");
+        const titleInputBar =  document.createElement("input");
+        titleInputBar.type = "text";
+        titleInputBar.value = titleContainer.innerHTML;
+        titleInputBar.classList.add("input");
+        targetContainer.replaceChild(titleInputBar, titleContainer)
+
+        // replace memo description to input bar
+        const descriptionContainer = targetContainer.querySelector(".memo__description");
+        const descriptionInputBar =  document.createElement("input");
+        descriptionInputBar.type = "text";
+        descriptionInputBar.value = descriptionContainer.innerHTML;
+        descriptionInputBar.classList.add("input");
+        targetContainer.replaceChild(descriptionInputBar, descriptionContainer)
+    } else {
+        const editedInputBars = targetContainer.querySelectorAll("input.input");
+
+        // edit on frontend Section
+
+        const editedTitleBox = document.createElement("h2");
+        editedTitleBox.className = "memo__title";
+        editedTitleBox.innerHTML = editedInputBars[0].value;
+
+        const editedDescriptionBox = document.createElement("h5");
+        editedDescriptionBox.className = "memo__description";
+        editedDescriptionBox.innerHTML = editedInputBars[1].value;
+
+        targetContainer.replaceChild(editedTitleBox, editedInputBars[0]);
+        targetContainer.replaceChild(editedDescriptionBox, editedInputBars[1]);
+
+        // edit on backend Section
+        const editedContainerId = targetContainer.id;
+        memoArray = memoArray.map(oneMemo => {
+            if (oneMemo.id === parseInt(editedContainerId)) {
+                return {
+                    title: editedInputBars[0].value,
+                    description: editedInputBars[1].value,
+                    id: oneMemo.id,
+                }
+            } else {
+                return oneMemo;
+            }
+        })
+        
+        localStorage.setItem("toDos", JSON.stringify(memoArray))
+    }
 }
 
 function paintMemo(obj) {
@@ -51,26 +142,27 @@ function paintMemo(obj) {
 
     // delete button
     const deleteBtn = document.createElement("button");
-    deleteBtn.className = "btn";
+    deleteBtn.classList.add("btn", "hide")
     const deleteIcon = document.createElement("i");
     deleteIcon.classList.add("fas", "fa-trash", "fa-lg");
     deleteBtn.appendChild(deleteIcon);
-
     deleteBtn.addEventListener("click", handleDeleteBtnClick);
 
     // edit button
     const editBtn = document.createElement("button");
-    editBtn.className = "btn";
+    editBtn.classList.add("btn", "hide");
     const editIcon = document.createElement("i");
     editIcon.classList.add("fas", "fa-pen", "fa-lg");
     editBtn.appendChild(editIcon);
+    editBtn.addEventListener("click", handleEditBtnClick);
 
     // option button
     const optionBtn = document.createElement("button");
     optionBtn.className = "btn";
     const optionIcon = document.createElement("i");
-    optionIcon.classList.add("fas", "fa-chevron-left", "fa-lg");
+    optionIcon.classList.add("fas", "fa-chevron-right", "fa-lg");
     optionBtn.appendChild(optionIcon);
+    optionBtn.addEventListener("click", handleOptionBtnClick);
 
     divBox.appendChild(titleBox);
     divBox.appendChild(descriptionBox);
@@ -117,7 +209,7 @@ function handleAddBtnClick(event) {
 function init() {
 
     // 기존의 localStorage 내용 로드
-    const loadedArray = localStorage.getItem("myMemo");
+    const loadedArray = localStorage.getItem("toDos");
     const parsedArray = JSON.parse(loadedArray);
     
 
