3bf2f7ccdebb022f5673d371300ba34c9fc22826
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-22 15:22:08 +0900

    Removed useless part

--- src/client/js/nav.js
+++ src/client/js/nav.js
@@ -1,49 +1,28 @@
 import { Btn, BtnActive } from "./btn"
- 
+
 // Class
 
 class Nav {
     constructor() {
         this.jsNavHeader = document.querySelector("#jsNavHeader");
-        this.jsNavBtn = document.querySelector("#jsNavBtn");
-        this.jsNavIcon = this.jsNavBtn ? (this.jsNavBtn.querySelector("i")) : null;
-        this.jsNavUl = document.querySelector("#jsNavUl");
-        this.jsNavLi = this.jsNavUl.querySelectorAll("li");
+        this.jsNavContainer = document.querySelector("#jsNavContainer");
+        this.jsNavMemo = this.jsNavContainer.querySelector("#jsNavMemo");
+        this.jsNavLoad = this.jsNavContainer.querySelector("#jsNavLoad");
         
-        if (this.jsNavBtn && this.jsNavUl) {
-            this.initNav();
+        // Btn & BtnActive Styling
+        new Btn(this.jsNavHeader);
+        new BtnActive(this.jsNavMemo);
+        new BtnActive(this.jsNavLoad);
 
-            // Btn Styling
-            new Btn(this.jsNavHeader);
-            
-            // BtnActive Styling
-            new BtnActive(this.jsNavBtn);
-
-            this.jsNavLi.forEach(li => {
-                new BtnActive(li);
-            })
+        // Functions
+        if (this.jsNavContainer) {
+            this.navInit();
         }
     }
 
-    // functions
-
-    initNav = () => {
-        this.jsNavBtn.addEventListener("click", this.handleNavBtn);
-    }
-
-    handleNavBtn = (event) => {
-        event.preventDefault();
-        this.jsNavBtn.classList.toggle("btn-clicked");
-        if (this.jsNavBtn.classList.contains("btn-clicked")) {
-            if (this.jsNavUl.classList.contains("init-hide")) {
-                this.jsNavUl.classList.replace("init-hide", "show-slider")
-            } else {
-                this.jsNavUl.classList.replace("hide-slider", "show-slider")
-            }
-        } else {
-            this.jsNavUl.classList.replace("show-slider", "hide-slider")
-        }
+    navInit = () => {
+        
     }
 }
 
-new Nav()
+new Nav();
