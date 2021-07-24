20575be0f569d159fe16fad36d2487cc33e73a7e
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-24 11:59:57 +0900

    필요없는 부분 삭제

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import morgan from "morgan";
-import session from "express-session";
-import sessionStore from "sessionstore";
 import { memo } from "./controllers/userController";
 
 const app = express();
@@ -12,15 +10,6 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-app.use(session({
-    secret: "Hello",
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore.createSessionStore({
-        type: "mongodb",
-    }),
-}));
 
 app.use("/static", express.static("assets"));
 
